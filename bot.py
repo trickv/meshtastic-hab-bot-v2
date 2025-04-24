9b051c225b964f16acc72d5e1e3c0139c97d59ab
Patrick van Staveren <trick@vanstaveren.us>
2025-04-24 21:32:12 +0000

    lots of error handling

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import time
 import datetime
 import math
 import json
+import traceback
 
 # WGS84 ellipsoid constants
 a = 6378137.0          # semi-major axis in meters
@@ -39,46 +40,66 @@ def distance_between_geodetic_points(p1, p2):
 # Received: {'from': 530607104, 'to': 131047185, 'decoded': {'portnum': 'TEXT_MESSAGE_APP', 'payload': b'G', 'bitfield': 1, 'text': 'G'}, 'id': 103172025, 'rxTime': 1745376860, 'rxSnr': 7.0, 'hopLimit': 7, 'wantAck': True, 'rxRssi': -14, 'hopStart': 7, 'publicKey': 'Jn89K4tEsX2fKYy+NUu3J8EJ/gjXjxP1SQCHm3A8Wms=', 'pkiEncrypted': True, 'raw': from: 530607104, to: 131047185, [...], 'fromId': '!1fa06c00', 'toId': '!07cf9f11'}
 
 def onReceive(packet, interface):
+    print("packet")
     #print(f"Received: {packet}")
     try:
         if packet['decoded']['portnum'] == 'TEXT_MESSAGE_APP' and packet['to'] == 131047185:
-            print(f"rx msg: {packet['decoded']['payload']}")
+            print(f"rx msg: {packet['decoded']['payload']} from {packet['fromId']}")
             rx_time = datetime.datetime.fromtimestamp(packet['rxTime']).time()
 
             msg = f"KD9PRC 🎈 node. "
-            msg += f"I ack your msg at {rx_time} SNR {packet['rxSnr']} RSSI {packet['rxRssi']}. My alt {round(pos['altitude'],0)}m, lat {round(pos['latitude'],3)} lon {round(pos['longitude'],3)}. "
-            if packet['fromId'] in interface.nodes:
-                print("fromId is in")
-                if 'position' in interface.nodes[packet['fromId']]:
-                    print("it has position")
-                    remote_position = interface.nodes[packet['fromId']]['position']
-                    if {'latitude', 'longitude', 'altitude'}.issubset(remote_position):
-                        print("it has attrs")
-                        distance_km = distance_between_geodetic_points(
-                            (remote_position['latitude'], remote_position['longitude'], remote_position['altitude']),
-                            (pos['latitude'], pos['longitude'], pos['altitude']))
-                        msg += f"We are {distance_km}km apart! "
+
+            msg += f"I ack your msg at {rx_time}. "
+            try:
+                msg += f"SNR {packet['rxSnr']} RSSI {packet['rxRssi']}. "
+            except Exception:
+                print("failed to get SNR?")
+                traceback.print_exc()
+            if {'latitude', 'longitude', 'altitude'}.issubset(pos):
+                print("I have local position")
+                msg += f"My alt {round(pos['altitude'],0)}m, lat {round(pos['latitude'],3)} lon {round(pos['longitude'],3)}. "
+                if packet['fromId'] in interface.nodes:
+                    print("fromId is in")
+                    if 'position' in interface.nodes[packet['fromId']]:
+                        print("it has position")
+                        remote_position = interface.nodes[packet['fromId']]['position']
+                        if {'latitude', 'longitude', 'altitude'}.issubset(remote_position):
+                            print("it has attrs")
+                            distance_km = distance_between_geodetic_points(
+                                (remote_position['latitude'], remote_position['longitude'], remote_position['altitude']),
+                                (pos['latitude'], pos['longitude'], pos['altitude']))
+                            msg += f"We are {distance_km}km apart! "
+                    else:
+                        print("No remote position available")
+            else:
+                print("No local GPS fix")
+                msg += "My GPS has no lock at the moment. "
             msg += "Thanks for QSO!"
             interface.sendText(msg, packet['from'])
             print(f"sent reply, {len(msg)}")
-    except Exception:
-        print('unhandled packet')
+    except Exception as e:
+        print('unhandled exception')
+        traceback.print_exc()
 
 pub.subscribe(onReceive, "meshtastic.receive")
 interface = meshtastic.tcp_interface.TCPInterface(hostname='127.0.0.1')
 
 while True:
     my = interface.getMyNodeInfo()
+    print(my)
     pos = my['position']
     pay = {
-        'alt': round(my['position']['altitude'],0),
-        'lat': round(my['position']['latitude'],4),
-        'lon': round(my['position']['longitude'],4),
         'chUtil': round(my['deviceMetrics']['channelUtilization'], 2),
         'airUtilTx': round(my['deviceMetrics']['channelUtilization'], 2),
         'uptime': my['deviceMetrics']['uptimeSeconds'],
+        }
+    if {'altitude', 'latitude', 'longitude'}.issubset(pos):
+        pay.update({
+            'alt': round(my['position']['altitude'],0),
+            'lat': round(my['position']['latitude'],4),
+            'lon': round(my['position']['longitude'],4),
+        })
 # TODO: linux uptime, Pi cpu temp?, some voltage?
-    }
     pay_json = json.dumps(pay)
     msg = f"mtf:{pay_json}"
     interface.sendText(msg, '!d9efdb3d')
